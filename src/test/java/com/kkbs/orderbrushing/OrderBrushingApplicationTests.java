ff43fa618ccbee68e1b6acaefa6f94275a05a32c
kevinngth <kevin.ng.th@gmail.com>
2020-06-13 17:15:28 +0800

    catch brushers

--- src/test/java/com/kkbs/orderbrushing/OrderBrushingApplicationTests.java
+++ src/test/java/com/kkbs/orderbrushing/OrderBrushingApplicationTests.java
@@ -16,6 +16,17 @@ class OrderBrushingApplicationTests {
 
 	String[] HEADERS = { "orderid", "shopid", "userid", "event_time"};
 
+	public String oneHourLater(String oldTime) {
+		String hh = oldTime.substring(11,13);
+		if (hh.equals("23")) {
+			hh = "00";
+		} else {
+			int h = Integer.parseInt(hh);
+			hh = h < 9 ? "0" + (h + 1) : String.valueOf(h + 1);
+		}
+		return oldTime.substring(0, 11) + hh + oldTime.substring(13);
+	}
+
 	@Test
 	public void shouldReadFile() throws IOException {
 		Reader in = new FileReader("src/main/resources/order_brush_order.csv");
@@ -37,13 +48,20 @@ class OrderBrushingApplicationTests {
 		}
 
 		Collections.sort(orderList);
-
+		ArrayList<Long> brushers = new ArrayList<>();
 		for (int i = 0; i < orderList.size(); i++) {
 			Order order = orderList.get(i);
-			if (order.getUserId() == 201343856) {
-				System.out.println(order.getUserId() + ", " + order.getEventTime());
+			int j = 1;
+			while ( i + j < orderList.size() && orderList.get(i+j).getEventTime().compareTo(oneHourLater(order.getEventTime())) < 0) {
+				if (order.getUserId() == orderList.get(i+j).getUserId()) {
+					if (!brushers.contains(order.getUserId())) {
+						brushers.add(order.getUserId());
+					}
+				}
+			j++;
 			}
 		}
+		System.out.println(brushers.toString());
 	}
 
 }
